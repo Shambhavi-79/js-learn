b93a68f49e8ab1225a134e0fdd95774594a2323d
Shambhavi Thakur <thakur.shambhavi14@gmail.com>
2026-05-27 13:22:36 +0000

    operations demo

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -33,9 +33,25 @@ console.log(typeof stringNumber); */
 
 let value = 3
 let negValue = -value
-console.log(negValue);
+// console.log(negValue);
 
 let str1 = "Hello"
 let str2 = " Shambhavi"
 let str3 = str1 + str2
-console.log(str3);
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);  //122
+// console.log(1 + 2 + "2");  //32
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
