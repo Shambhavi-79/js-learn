cdb6127587926777f8dc5429445c48f89f18ac03
Shambhavi Thakur <thakur.shambhavi14@gmail.com>
2026-05-28 13:22:34 +0000

    dates in JS

--- 01_Basics/07_dates.js
+++ 01_Basics/07_dates.js
@@ -1,12 +1,37 @@
 // Dates
 
 let myDate = new Date()
-console.log(myDate.toString())
-console.log(myDate.toDateString())
-console.log(myDate.toISOString())
-console.log(myDate.toJSON())
-console.log(myDate.toLocaleDateString())
-console.log(myDate.toLocaleString())
-console.log(myDate.toLocaleTimeString())
-console.log(myDate.toTimeString())
-console.log(myDate.toUTCString())
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toUTCString())
+
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2026, 0, 23, 17, 5)
+//months in JS starts from 0
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getMonth() + 1);
+
+// ${newDate.getDay()} and the time
+
+console.log(newDate.toLocaleString('default', {
+    weekday: "long"
+}))
