d9ed681fd1b5e55afba337ca5117e85ba71bcec3
Shambhavi Thakur <thakur.shambhavi14@gmail.com>
2026-05-28 09:04:50 +0000

    dates in JS

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,12 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toTimeString())
+console.log(myDate.toUTCString())
