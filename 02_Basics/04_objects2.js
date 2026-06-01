8966ce828cab0133c690c5ae3d92094b63fcf8eb
Shambhavi Thakur <thakur.shambhavi14@gmail.com>
2026-06-01 09:45:40 +0000

    objects in depth

--- 02_Basics/04_objects2.js
+++ 02_Basics/04_objects2.js
@@ -45,3 +45,12 @@ const users = [
         email: "c@gmail.com"
     }
 ]
+
+// console.log(users[2].email)
+// console.log(tinderUser)
+
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLogged'))
