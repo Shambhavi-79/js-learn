8c2553e06709699e18134aefb9735dd710a22fe9
Shambhavi Thakur <thakur.shambhavi14@gmail.com>
2026-06-03 09:18:01 +0000

    more about functions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -37,4 +37,34 @@ function loginUserMessage(username = "Sachin"){
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("Shambhavi"))
+// console.log(loginUserMessage("Shambhavi"))
+
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500))
+
+const user = {
+    username: "Sachin",
+    price: 200
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username: "Shambhavi",
+    price: 400
+})
+
+const myNewArray = [100, 200, 300, 400]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([100, 200, 300, 400]))
