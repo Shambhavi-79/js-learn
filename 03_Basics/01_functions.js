0d7f23783045cd8d838029923d7e25bf24ba066c
Shambhavi Thakur <thakur.shambhavi14@gmail.com>
2026-06-03 07:29:07 +0000

    functions in js

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,17 @@
+// console.log("S")
+// console.log("a")
+// console.log("c")
+// console.log("h")
+// console.log("i")
+// console.log("n")
+
+function sayMyName(){
+    console.log("S")
+    console.log("a")
+    console.log("c")
+    console.log("h")
+    console.log("i")
+    console.log("n")
+}
+
+sayMyName()
