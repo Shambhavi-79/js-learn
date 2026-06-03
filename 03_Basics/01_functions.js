9a26771814294bb9c9d5041fd20a4677cda893fb
Shambhavi Thakur <thakur.shambhavi14@gmail.com>
2026-06-03 08:52:55 +0000

    functions in js

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -14,4 +14,27 @@ function sayMyName(){
     console.log("n")
 }
 
-sayMyName()
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2)
+// } 
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    // console.log("This will never be printed")
+    return number1 + number2
+} 
+
+const result = addTwoNumbers(1, 8)
+
+// console.log("Result:", result)
+
+function loginUserMessage(username = "Sachin"){
+    if (!username){
+        console.log("Please enter a username")
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("Shambhavi"))
