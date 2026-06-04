eed9ec0024ac46372fe9b2f75360f7b9ac4ce57f
Shambhavi Thakur <thakur.shambhavi14@gmail.com>
2026-06-04 13:47:44 +0000

    scopes

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -6,9 +6,54 @@ let a = 300
 if (true){
     let a = 10 
     const b = 20
-    console.log("Inner: ",a)
+    // console.log("Inner: ",a)
 }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+
+// nested scopes
+
+function one(){
+    const username = "shambhavi"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username)
+    }
+    // console.log(website)
+
+    two()
+}
+
+one()
+
+if (true){
+    const username = "shambhavi"
+    if (username === "shambhavi"){
+        const website = " youtube"
+        // console.log(username + website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+
+
+
+//********* interesting*********
+
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+addtwo(5))    //when printed, shows error
+const addtwo = function(num){
+    return num + 2
+}
+
